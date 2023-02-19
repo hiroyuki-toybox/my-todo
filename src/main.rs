15310afe40ef74d9d98b11a53f12f34f2754105b
hir0o <mk.9.q.kyu@gmail.com>
2023-02-19 17:39:59 +0900

    TodoRepositoryを作成

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+mod handlers;
+mod repositories;
+
+use axum::routing::{get, post};
+use axum::{extract::Extension, Router};
+use handlers::create_todo;
+use repositories::TodoRepository;
 use std::env;
 use std::net::SocketAddr;
+use std::sync::Arc;
+
+use crate::repositories::TodoRepositoryForMemory;
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +18,8 @@ async fn main() {
     env::set_var("Rust_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    let app = create_app();
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
 
@@ -25,76 +29,32 @@ async fn main() {
         .unwrap();
 }
 
-fn create_app() -> Router {
+fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user))
+        .route("/todos", post(create_todo::<T>))
+        // axumアプリケーション内でrepositoryを共有する
+        .layer(Extension(Arc::new(repository)))
 }
 
 async fn root() -> &'static str {
     "Hello, world!!"
 }
 
-async fn create_user(
-    // ここでDeserialize
-    Json(payload): Json<CreateUser>,
-) -> impl IntoResponse {
-    let user = User {
-        id: 1223,
-        username: payload.username,
-    };
-
-    (StatusCode::CREATED, Json(user))
-}
-
-#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-    use axum::{
-        body::Body,
-        http::{header, Method, Request},
-    };
+    use axum::{body::Body, http::Request};
 
     use tower::ServiceExt;
 
     #[tokio::test]
     async fn should_return_hello_world() {
+        let repository = TodoRepositoryForMemory::new();
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body = String::from_utf8(bytes.to_vec()).unwrap();
         assert_eq!(body, "Hello, world!!");
-    }
-
-    #[tokio::test]
-    async fn should_return_user_data() {
-        let req = Request::builder()
-            .uri("/users")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(r#"{"username": "test"}"#))
-            .unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
-        let user: User = serde_json::from_str(&body).unwrap();
-        assert_eq!(
-            user,
-            User {
-                id: 1223,
-                username: "test".to_string()
-            }
-        );
     }
 }
