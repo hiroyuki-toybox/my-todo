e6be0418c17423b6298bf73c2c063d2f927b9922
hir0o <mk.9.q.kyu@gmail.com>
2023-02-22 08:15:12 +0900

    メモリに保存

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,15 +1,16 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, RwLock},
+    sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
 };
 
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 enum RepositoryError {
     #[error("NotFound, id is {0}")]
-    NotFound(u64),
+    NotFound(i32),
 }
 
 pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
@@ -61,22 +62,97 @@ impl TodoRepositoryForMemory {
             store: Arc::default(),
         }
     }
+
+    fn write_store_ref(&self) -> RwLockWriteGuard<TodoDatas> {
+        self.store.write().unwrap()
+    }
+
+    fn read_store_ref(&self) -> RwLockReadGuard<TodoDatas> {
+        self.store.read().unwrap()
+    }
 }
 
 impl TodoRepository for TodoRepositoryForMemory {
     fn create(&self, payload: CreateTodo) -> Todo {
-        todo!()
+        let mut store = self.write_store_ref();
+        let id = (store.len() + 1) as i32;
+        let todo = Todo::new(id, payload.text.clone());
+        store.insert(id, todo.clone());
+        todo
     }
     fn find(&self, id: i32) -> Option<Todo> {
-        todo!()
+        let store = self.read_store_ref();
+        store.get(&id).map(|todo| todo.clone())
     }
     fn all(&self) -> Vec<Todo> {
-        todo!()
+        let store = self.read_store_ref();
+        Vec::from_iter(store.values().map(|todo| todo.clone()))
     }
     fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
-        todo!()
+        let mut store = self.write_store_ref();
+        let todo = store.get(&id).context(RepositoryError::NotFound(id))?;
+        let text = payload.text.unwrap_or(todo.text.clone());
+        let completed = payload.completed.unwrap_or(todo.completed);
+
+        let todo = Todo {
+            id,
+            text,
+            completed,
+        };
+        store.insert(id, todo.clone());
+        Ok(todo)
     }
     fn delete(&self, id: i32) -> anyhow::Result<()> {
-        todo!()
+        let mut store = self.write_store_ref();
+        store.remove(&id).ok_or(RepositoryError::NotFound(id))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn todo_curd_scenario() {
+        let text = "todo text".to_string();
+        let id = 1;
+        let expected = Todo::new(id, text.clone());
+
+        // create
+        let repository = TodoRepositoryForMemory::new();
+        let todo = repository.create(CreateTodo { text: text.clone() });
+        assert_eq!(todo, expected);
+
+        // find
+        let todo = repository.find(id).unwrap();
+        assert_eq!(todo, expected);
+
+        // all
+        let todos = repository.all();
+        assert_eq!(todos, vec![expected.clone()]);
+
+        // update
+        let text = "update todo".to_string();
+        let todo = repository.update(
+            id,
+            UpdateTodo {
+                text: Some(text.clone()),
+                completed: None,
+            },
+        );
+
+        let expected = Todo {
+            id,
+            text,
+            completed: false,
+        };
+
+        assert_eq!(todo.unwrap(), expected);
+
+        // delete
+        repository.delete(id).unwrap();
+        let todo = repository.find(id);
+        assert_eq!(todo, None);
     }
 }
