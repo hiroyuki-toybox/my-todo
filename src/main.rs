b9b52cd052a29bef91d1003b115b102f15fb1ecc
hir0o <mk.9.q.kyu@gmail.com>
2023-04-19 21:52:27 +0900

    ハンドラのテストを作成

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,8 @@ mod test {
     use axum::response::Response;
     use axum::{body::Body, http::Request};
 
-    use hyper::{header, Method};
+    use hyper::{header, Method, StatusCode};
+    use serde::de::Expected;
     use tower::ServiceExt;
 
     fn build_todo_req_with_json(path: &str, method: Method, json_body: String) -> Request<Body> {
@@ -74,11 +75,91 @@ mod test {
             .unwrap()
     }
 
+    async fn res_to_string(res: Response) -> String {
+        let b = res.into_body();
+        let bytes = hyper::body::to_bytes(b).await.unwrap();
+        String::from_utf8(bytes.to_vec()).unwrap()
+    }
+
     async fn res_to_todo(res: Response) -> Todo {
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        let body = res_to_string(res).await;
         let todo: Todo = serde_json::from_str(&body).expect(&format!("body: {}", body));
         todo
+    }
+
+    #[tokio::test]
+    async fn should_created_todo() {
+        let expected = Todo::new(1, "should_created_todo".to_string());
+
+        let repository = TodoRepositoryForMemory::new();
+        let req = build_todo_req_with_json(
+            "/todos",
+            Method::POST,
+            r#"{" "text": "should_created_todo" }"#.to_string(),
+        );
+
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let todo = res_to_todo(res).await;
+        assert_eq!(expected, todo);
+    }
+
+    #[tokio::test]
+    async fn should_find_todo() {
+        // 期待値作成
+        let expected = Todo::new(1, "should_find_todo".to_string());
+        // repo作成
+        let repository = TodoRepositoryForMemory::new();
+        // repoから、Todoを作成
+        repository.create(CreateTodo::new("should_created_todo".to_string()));
+        // リクエストを作成
+        let req = build_todo_req_with_empty("/todo/1", Method::GET);
+        // レスポンスを作成
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        // レスポンスから、todoを生成
+        let todo = res_to_todo(res).await;
+        // expected
+        assert_eq!(expected, todo);
+    }
+
+    #[tokio::test]
+    async fn should_get_all_todos() {
+        let expected = Todo::new(1, "should_get_all_todos".to_string());
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("should_get_all_todos".to_string()));
+        let req = build_todo_req_with_empty("/todos", Method::GET);
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let body = res_to_string(res).await;
+        let todo: Vec<Todo> = serde_json::from_str(&body)
+            .expect(&format!("connot convert TOdo instance. boy: {}", body));
+        assert_eq!(vec![expected], todo)
+    }
+
+    #[tokio::test]
+    async fn should_update_todo() {
+        let expected = Todo::new(1, "should_update_todo".to_string());
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("before_should_update_todo".to_string()));
+
+        let req = build_todo_req_with_json(
+            "/todos/1",
+            Method::PATCH,
+            r#"{" "text": "should_update_todo" }"#.to_string(),
+        );
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let todo = res_to_todo(res).await;
+
+        assert_eq!(expected, todo);
+    }
+
+    #[tokio::test]
+    async fn should_delete_todo() {
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("should_delete_todo".to_string()));
+
+        let req = build_todo_req_with_empty("/todos/1", Method::DELETE);
+        let res = create_app(repository).oneshot(req).await.unwrap();
+
+        assert_eq!(StatusCode::NO_CONTENT, res.status());
     }
 
     #[tokio::test]
